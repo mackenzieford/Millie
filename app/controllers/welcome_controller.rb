134f33227cd1426adaeefef3f9c3d4be7b922cc7
kenzford8@gmail.com <kenzford8@gmail.com>
2019-06-19 15:28:35 +0000

    m

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
   def index
   end
+  
+  def p1
+  end
+  
+  def parents
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   get 'welcome/p1'
+  get 'welcome/p2'
   
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
